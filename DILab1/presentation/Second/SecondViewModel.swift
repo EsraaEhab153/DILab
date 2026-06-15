8a933640eccf4cb1dd8498a00ab9c2ba6a619a46
Esraa Ehba <esraaehab530@gmail.com>
2026-06-15 08:40:22 -0700

    finishd day 2 lab

---
+++ DILab1/presentation/Second/SecondViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MovieDetailsViewModel.swift
+//  DILab1
+//
+//  Created by Esraa Ehab on 15/06/2026.
+//
+
+import Foundation
+class SecondViewModel {
+    let movieName: String
+    
+    init(movieName: String) {
+        self.movieName = movieName
+    }
+}
---
+++ DILab1/presentation/ViewControllerFactory.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllerFactory.swift
+//  DILab1
+//
+//  Created by Esraa Ehab on 15/06/2026.
+//
+
+import Foundation
